dd0c0a277738702b2f034fbd1ca8515e651d5d1e
AdamCerutti <46689357+AdamCerutti@users.noreply.github.com>
2022-05-10 14:18:35 -0400

    Initial InsertRunnable.java

---
+++ InsertRunnable.java
@@ -0,0 +1,24 @@
+package chess;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class InsertRunnable implements Runnable {
+    private String query;
+    
+    public InsertRunnable(String q) {
+        this.query = q;
+    }
+    
+    public void run() {
+        Connection tmp = new ConnectToDB().EstablishConnection();
+		    try {
+			    Statement insert = tmp.createStatement();
+		  	  insert.execute(this.query);
+	  		  insert.close();
+	  	  } catch (SQLException e) {
+		  	  e.printStackTrace();
+		    }
+    }
+}
